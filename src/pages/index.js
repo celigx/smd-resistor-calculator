00da6a1b81ef642dada15351a035631c138956e2
Celiox <cela@protonmail.com>
2020-12-03 10:58:44 +0100

    Add input

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [value, setValue] = useState('');
+  
+  // Update input change
+  const valueChange = (e) => {
+    setValue(e.target.value)
+  }
+  console.log('input:', value)
+
+  return (
+  <div>
+    <h1>Enter SMD Code:</h1>
+    <input value={value} maxLength='4' onChange={valueChange} />
+  </div>
+  )
 }
