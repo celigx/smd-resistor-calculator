d73ddead731b2c22adb95d7e4a148b74ea636d0f
Celiox <cela@protonmail.com>
2020-12-03 11:19:38 +0100

    Add calculate function

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,34 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 export default function Home() {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState('')
+  const [output, setOutput] = useState('')
+
+  useEffect(() => {
+    calculateSMD()
+  })
   
   // Update input change
   const valueChange = (e) => {
     setValue(e.target.value)
   }
-  console.log('input:', value)
+
+  const calculateSMD = () => {
+    // Remove last number
+    const mainNumber = parseInt(value.toString().slice(0, -1))
+    // Get last number
+    const lastNumber = parseInt(value.toString().split('').pop())
+    // Calculate SMD
+    const calculate = mainNumber * Math.pow(10, lastNumber)
+
+    setOutput(calculate)
+  }
 
   return (
   <div>
     <h1>Enter SMD Code:</h1>
     <input value={value} maxLength='4' onChange={valueChange} />
+    <h1>{output}</h1>
   </div>
   )
 }
